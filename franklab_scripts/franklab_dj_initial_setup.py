39044e1995ffc8a22a79301c31e999678ba8537b
Loren Frank <loren@phy.ucsf.edu>
2020-11-15 16:21:38 -0800

    renamed initial setup script

---
+++ franklab_scripts/franklab_dj_initial_setup.py
@@ -0,0 +1,18 @@
+# run this script after activating the nwb_datajoint environment and a mysql login has been created for you
+
+
+import datajoint as dj
+
+dj.config['database.host'] = "lmf-db.cin.ucsf.edu"
+dj.config['database.user'] = "user_name_here"
+
+# change your password
+dj.set_password()
+
+dj.config['database.password'] = "password_here"
+
+dj.config.save_global()
+
+
+
+
