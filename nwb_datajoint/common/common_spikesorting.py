c5aaf34c3d51990dbb19b6fed1a4871dd6eb434c
Kyu Hyun Lee <kyuhyun9056@gmail.com>
2020-12-09 15:47:06 -0800

    Change key name

--- nwb_datajoint/common/common_spikesorting.py
+++ nwb_datajoint/common/common_spikesorting.py
@@ -872,11 +872,11 @@ class CuratedSpikeSorting(dj.Computed):
 
     def make(self, key):
         parent_key = (SpikeSorting & key).fetch1()
-        key['analysis_file_name'] = parent_key['nwb_file_name']
+        key['nwb_file_name'] = parent_key['nwb_file_name']
         key['curation_feed_uri'] = parent_key['curation_feed_uri']
         self.insert1(key)
         labels = self.get_labels(key['curation_feed_uri'])
-        self.add_labels_analysisNWB(key['analysis_file_name'], key['curation_feed_uri'])
+        self.add_labels_analysisNWB(key['nwb_file_name'], key['curation_feed_uri'])
         # TODO add metrics to Units table; get them from analysisNWB file
         CuratedSpikeSorting.Units.insert(
             [dict(key, unit_id=unitId, label=label) for unitId,label in labels.items()])
