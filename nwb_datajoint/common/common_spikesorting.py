910987f94ffb9edafc3b28fe1d1451bee959ea7e
Kyu Hyun Lee <kyuhyun9056@gmail.com>
2020-12-14 11:30:15 -0800

    Debug

--- nwb_datajoint/common/common_spikesorting.py
+++ nwb_datajoint/common/common_spikesorting.py
@@ -961,6 +961,7 @@ class CuratedSpikeSorting(dj.Computed):
 
         with pynwb.NWBHDF5IO(path=Nwbfile.get_abs_path(analysis_file_name), mode="a") as io:
             nwbf=io.read()
+            print(len(nwbf.units))
             nwbf.add_unit_column(name='label', description='label given to unit during curation',
                                  data=list(labels.values()))
         return None
