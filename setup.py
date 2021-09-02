fe4142934dc788aecf62e9ae1ebaaac20673a2ff
Loren Frank <loren@phy.ucsf.edu>
2021-09-01 19:29:17 -0700

    new setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
-import setuptools
-
-setuptools.setup()
+from setuptools import setup, find_packages
+  
+setup(
+    packages=find_packages(),
+    include_package_data = True,
+    install_requires=[
+        'pynwb',
+        'sortingview>=0.6'
+    ]
+)
