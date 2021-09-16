a487cbdff2b37aca288d7769acd39899a6303d8c
Loren <loren@phy.ucsf.edu>
2021-09-16 10:20:57 -0700

    add_dj_module to add all kachery-users as rw for schema set

---
+++ config/add_dj_module.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import os
+import sys
+import grp
+import tempfile
+
+target_group = 'kachery-users'
+
+def add_module(module_name):
+    print(f'Granting everyone permissions to module {module_name}')
+    
+    # create a tempoary file for the command
+    file = tempfile.NamedTemporaryFile(mode='w')
+
+    # find the kachery-users group 
+    groups = grp.getgrall()
+    for group in groups:
+        if grouop.gr_name == target_group:
+            break
+
+    # get a list of usernames 
+    for user in group.members:
+        file.write(
+            f"GRANT ALL PRIVILEGES ON `{module_name}\_%`.* TO `{user}`@'%';\n")
+    file.flush()
+
+    # run those commands in sql
+    os.system(f'mysql -p -h lmf-db.cin.ucsf.edu < {file.name}')
+
+
+if __name__ == "__main__":
+    add_module(sys.argv[1])
