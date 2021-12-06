816cbc1a48fe5d2a70ba2360ad1f096173735262
Kyu Hyun Lee <kyuhyun9056@gmail.com>
2021-12-05 16:06:19 -0800

    Update waveform schema

--- src/nwb_datajoint/common/common_waveforms.py
+++ src/nwb_datajoint/common/common_waveforms.py
@@ -25,12 +25,13 @@ class WaveformParameters(dj.Manual):
     definition = """
     list_name: varchar(80) # name of waveform extraction parameters
     ---
-    params: blob # a dictionary containing waveform extraction parameters
+    params: blob # a dict of waveform extraction parameters
     """
     def insert_default_params(self):
         key = {}
-        self.insert1(key)
-        
+        key['list_name'] = 'default'
+        key['params'] = {'ms_before':1, 'ms_after':1, 'max_spikes_per_unit':1000}
+        self.insert1(key) 
 
 @schema
 class WaveformSelection(dj.Manual):
@@ -48,4 +49,33 @@ class Waveforms(dj.Computed):
     -> AnalysisNwbfile
     """
     def make(self, key):
+        recording = key['recording']
+        sorting = key['sorting']
+        save_path = key['save_path']
+        params = key['params']
+        si.extract_waveforms(recording=recording, sorting=sorting, folder=save_path, **params)
+        # add to nwb file
+        # nwbfile = pynwb.NWBFile(...)
+        # wfs = [
+        #         [     # elec 1
+        #             [1, 2, 3],  # spike 1, [sample 1, sample 2, sample 3]
+        #             [1, 2, 3],  # spike 2
+        #             [1, 2, 3],  # spike 3
+        #             [1, 2, 3]   # spike 4
+        #         ], [  # elec 2
+        #             [1, 2, 3],  # spike 1
+        #             [1, 2, 3],  # spike 2
+        #             [1, 2, 3],  # spike 3
+        #             [1, 2, 3]   # spike 4
+        #         ], [  # elec 3
+        #             [1, 2, 3],  # spike 1
+        #             [1, 2, 3],  # spike 2
+        #             [1, 2, 3],  # spike 3
+        #             [1, 2, 3]   # spike 4
+        #         ]
+        # ]
+        # elecs = ... # DynamicTableRegion referring to three electrodes (rows) of the electrodes table
+        # nwbfile.add_unit(spike_times=[1, 2, 3], electrodes=elecs, waveforms=wfs)
+        
+        # key['analysis_nwb_file'] = analysis_nwb_file_path
         self.insert1(key)
