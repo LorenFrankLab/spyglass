fe1b377d5722d76972bb6aca28a74d6407902f14
Kyu Hyun Lee <kyuhyun9056@gmail.com>
2021-12-30 00:35:40 -0800

    Finish adding metric to workspace

--- src/nwb_datajoint/common/sorting_utils.py
+++ src/nwb_datajoint/common/sorting_utils.py
@@ -7,10 +7,14 @@ import kachery_client as kc
 import spikeinterface as si
 import spikeinterface.toolkit as st
 import sortingview as sv
+import spikeextractors
 from .common_lab import LabMember
 from .common_interval import IntervalList
 from .common_nwbfile import AnalysisNwbfile
 from .common_spikesorting import SpikeSortingRecording, SpikeSortingWorkspace, SortingID
+
+from pathlib import Path
+import tempfile
 
 def store_sorting_nwb(key, *, sorting, sort_interval_list_name, sort_interval, metrics=None, unit_ids=None):
     """store a sorting in a new AnalysisNwbfile
@@ -112,35 +116,68 @@ def add_metrics_to_workspace(workspace_uri: str, sorting_id: str=None,
     -------
     url : str
         new URL for the view
+    external_metrics :  dict
+        metrics that were added to workspace
     """
-    si.set_global_tmp_folder(os.environ['KACHERY_TEMP_DIR'])
 
     workspace = sv.load_workspace(workspace_uri)
+    
     recording_id = workspace.recording_ids[0]
     recording = workspace.get_recording_extractor(recording_id=recording_id)
+
+    # first cache as binary using old spikeextractors to make it compatible with new si
+    tmpdir = tempfile.TemporaryDirectory(dir=os.environ['KACHERY_TEMP_DIR'])
+    cached_recording = spikeextractors.CacheRecordingExtractor(recording, 
+                                                               save_path=str(Path(tmpdir.name) / 'r.dat'))
+
+    # then load with spikeinterface
+    new_recording = si.read_binary(file_paths=str(Path(tmpdir.name) / 'r.dat'),
+                                   sampling_frequency=cached_recording.get_sampling_frequency(),
+                                   num_chan=cached_recording.get_num_channels(),
+                                   dtype=cached_recording.get_dtype(),
+                                   time_axis=0,
+                                   is_filtered=True)
+    new_recording.set_channel_locations(locations=recording.get_channel_locations())
+
     if sorting_id is None:
         sorting_id = workspace.sorting_ids[0]
     sorting = workspace.get_sorting_extractor(sorting_id=sorting_id)
-    recording = si.core.create_recording_from_old_extractor(recording)
-    sorting = si.core.create_sorting_from_old_extractor(sorting)
-    recording = recording.save()
-    sorting = sorting.save()
-    waveforms = si.extract_waveforms(recording, sorting, folder=os.environ['KACHERY_TEMP_DIR'],
+    new_sorting = si.core.create_sorting_from_old_extractor(sorting)
+    # save sorting to enable parallelized waveform extraction
+    new_sorting = new_sorting.save(folder=str(Path(tmpdir.name) / 's'),)
+
+    waveforms = si.extract_waveforms(new_recording, new_sorting, folder=str(Path(tmpdir.name) / 'wf'),
                                      ms_before=1,ms_after=1, max_spikes_per_unit=2000,
-                                     return_scaled=True, n_jobs=5, total_memory='5G')
-    
+                                     overwrite=True,return_scaled=True, n_jobs=5, total_memory='5G')
+
     isolation = {}
     noise_overlap = {}
     for unit_id in sorting.get_unit_ids():
         isolation[unit_id] = st.qualitymetrics.pca_metrics.nearest_neighbors_isolation(waveforms, this_unit_id=unit_id)
         noise_overlap[unit_id] = st.qualitymetrics.pca_metrics.nearest_neighbors_noise_overlap(waveforms, this_unit_id=unit_id)
-    workspace.set_unit_metrics_for_sorting(sorting_id=sorting_id, metrics=[isolation, noise_overlap])
     
+    # external metrics must be in this format to be added to workspace
+    external_metrics = [{'name': 'isolation', 'label': 'isolation', 'tooltip': 'isolation',
+                        'data': isolation},
+                        {'name': 'noise_overlap', 'label': 'noise_overlap', 'tooltip': 'noise_overlap',
+                        'data': noise_overlap}
+                        ] 
+    workspace.set_unit_metrics_for_sorting(sorting_id=sorting_id, metrics=external_metrics)
+
     if user_ids is not None:
-        workspace.set_sorting_curation_authorized_users(sorting_id=sorting_id, user_ids=user_ids)
+        for user_id in user_ids:
+            workspace.set_user_permissions(user_id, {'edit': True})
 
-    url = workspace.experimental_spikesortingview(recording_id=recording_id, sorting_id=sorting_id,
-                                                  label=workspace.label, include_curation=True)
-    print(f'go to this URL for the new view: {url}')
+    F = workspace.figurl()
+    url = F.url(label=workspace.label, channel='franklab2')
 
-    return url
+    # in the future (once new view works fully):
+    # workspace.set_sorting_curation_authorized_users(sorting_id=sorting_id, user_ids=user_ids)
+    # url = workspace.experimental_spikesortingview(recording_id=recording_id, sorting_id=sorting_id,
+    #                                               label=workspace.label, include_curation=True)
+
+    print(f'URL for sortingview: {url}')
+    
+    return url, external_metrics
+
+
