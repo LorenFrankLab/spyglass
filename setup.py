2c73151c72e386c772e6941682fc5d44f55ca44b
Jeremy Magland <jeremy.magland@gmail.com>
2022-03-03 12:07:40 -0500

    move pip dependencies to setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,16 @@
 from setuptools import setup, find_packages
   
-setup()
+setup(
+    install_requires=[
+        'kachery-client>=1.1.7',
+        'mountainsort4',
+        'spikeextractors',
+        'pynwb>=2.0.0,<3',
+        'hdmf>=3.1.1,<4',
+        'datajoint>=0.13.*',
+        'ghostipy',
+        'pymysql>=1.0.*',
+        'h5py==2.10.*',
+        'sortingview>=0.7.3'
+    ]
+)
