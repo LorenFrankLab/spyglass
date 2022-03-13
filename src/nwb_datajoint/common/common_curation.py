51d9a70af5c04212a327c79686b7d2c950765af1
Cristofer <cristofer.holobetz@berkeley.edu>
2022-03-12 18:45:22 -0800

    Reject based on isi_violation_frac and noise_overlap in the same line. Change _sorting_after_reject output name to be noise_passed_sorting.

--- src/nwb_datajoint/common/common_curation.py
+++ src/nwb_datajoint/common/common_curation.py
@@ -152,32 +152,21 @@ class AutomaticCurationSorting(dj.Computed):
     @staticmethod
     def _sorting_after_reject(sorting, quality_metrics, reject_params):
        
-        # Reject units based off of noise_overlap
-        # A dict where each key is a unit_id and its value is a noise overlap score
+        # reject units based off of noise_overlap and isi_violation
+        # dicts where each key is a unit_id and values are metrics for that unit
         nn_noise_overlaps = quality_metrics['nn_noise_overlap']
-        
-        # Not sure what this name ('nn_noise_overlap_threshold') should be
-        nn_noise_overlap_threshold = reject_params['nn_noise_overlap_threshold']
-        
-        # Remove units that had too high a noise overlap. Keep the rest
-        nn_noise_overlap_passed_units_ids = [unit_id for unit_id in sorting.get_unit_ids() if nn_noise_overlaps[unit_id] < nn_noise_overlap_threshold]
-        
-        nn_noise_overlap_passed_sorting = sorting.select_units(nn_noise_overlap_passed_units_ids)
-                
-        # Reject units based off of isi_violation
-        # A dict where each key is a unit_id and its value is a fraction
-        # of spikes that violate the isi [0, 1]
         isi_violation_fractions = quality_metrics['isi_violation']
-        
-        # Not sure what this name ('isi_violation_frac_threshold') should be
+
+        # user-specified tolerances for noise overlap and isi violations
+        nn_noise_overlap_threshold = reject_params['nn_noise_overlap_threshold']
         isi_violation_frac_threshold = reject_params['isi_violation_frac_threshold']
+                
+        # remove units that had high noise_overlap or isi_violation_fracs. Keep the rest
+        noise_passed_units_ids = [unit_id for unit_id in sorting.get_unit_ids() if (nn_noise_overlaps[unit_id] < nn_noise_overlap_threshold) and (isi_violation_fractions[unit_id] < isi_violation_frac_threshold)]
         
-        # Remove units that had too many isi violations. Keep the rest
-        isi_passed_units_ids = [unit_id for unit_id in nn_noise_overlap_passed_sorting.get_unit_ids() if isi_violation_fractions[unit_id] < isi_violation_rate_threshold]
+        noise_passed_sorting = sorting.select_units(noise_passed_units_ids)
         
-        isi_noise_overlap_passed_sorting = nn_noise_overlap_passed_sorting.select_units(isi_passed_units_ids)
-        
-        return isi_noise_overlap_passed_sorting
+        return noise_passed_sorting
     
     @staticmethod
     def _sorting_after_merge(sorting, quality_metrics, merge_params):
