a01a61dccfa79af58bb6332be9823cfeecae0a88
Eric Denovellis <edeno@bu.edu>
2022-03-17 14:38:36 -0700

    Clean up init formatting

--- src/nwb_datajoint/common/__init__.py
+++ src/nwb_datajoint/common/__init__.py
@@ -1,3 +1,6 @@
+import os
+
+from .common_backup import CuratedSpikeSortingBackUp, SpikeSortingBackUp
 from .common_behav import (HeadDir, LinPos, PositionSource, RawPosition, Speed,
                            StateScriptFile, VideoFile)
 from .common_device import CameraDevice, DataAcquisitionDevice, Probe
@@ -5,32 +8,27 @@ from .common_dio import DIOEvents
 from .common_ephys import (LFP, Electrode, ElectrodeGroup, LFPBand,
                            LFPBandSelection, LFPSelection, Raw, SampleCount)
 from .common_filter import FirFilter
-from .common_interval import (IntervalList, SortInterval,
-                              intervals_by_length,
+from .common_interval import (IntervalList, SortInterval, interval_list_censor,
                               interval_list_contains,
                               interval_list_contains_ind,
                               interval_list_excludes,
                               interval_list_excludes_ind,
-                              interval_list_intersect,
-                              interval_list_censor,
-                              interval_list_union)
+                              interval_list_intersect, interval_list_union,
+                              intervals_by_length)
 from .common_lab import Institution, Lab, LabMember, LabTeam
 from .common_nwbfile import (AnalysisNwbfile, AnalysisNwbfileKachery, Nwbfile,
                              NwbfileKachery)
-from .common_position import (PositionInfoParameters, IntervalPositionInfoSelection, IntervalPositionInfo, LinearizationParameters, 
-                              TrackGraph, IntervalLinearizationSelection, IntervalLinearizedPosition, PositionVideo)
+from .common_position import (IntervalLinearizationSelection,
+                              IntervalLinearizedPosition, IntervalPositionInfo,
+                              IntervalPositionInfoSelection,
+                              LinearizationParameters, PositionInfoParameters,
+                              PositionVideo, TrackGraph)
 from .common_region import BrainRegion
 from .common_sensors import SensorData
 from .common_session import ExperimenterList, Session, SessionGroup
 from .common_subject import Subject
 from .common_task import Task, TaskEpoch
-from .common_backup import SpikeSortingBackUp, CuratedSpikeSortingBackUp
 from .nwb_helper_fn import (close_nwb_files, estimate_sampling_rate,
                             get_data_interface, get_electrode_indices,
                             get_nwb_file, get_raw_eseries, get_valid_intervals)
 from .populate_all_common import populate_all_common
-
-import os
-
-#si.set_global_tmp_folder(os.environ['NWB_DATAJOINT_TEMP_DIR'])
-
