eeedb196d4fe0d2969a8839029ab964b5ce67284
Eric Denovellis <edenovellis@gmail.com>
2022-03-24 12:04:38 -0700

    Minor formatting

--- src/nwb_datajoint/spikesorting/__init__.py
+++ src/nwb_datajoint/spikesorting/__init__.py
@@ -1,13 +1,17 @@
 from .sortingview import SortingviewWorkspace, SortingviewWorkspaceSelection
-from .spikesorting_recording import (SortGroup, SortInterval, SpikeSortingPreprocessingParameters,
-                                     SpikeSortingRecording, SpikeSortingRecordingSelection)
+from .spikesorting_recording import (SortGroup, SortInterval,
+                                     SpikeSortingPreprocessingParameters,
+                                     SpikeSortingRecording,
+                                     SpikeSortingRecordingSelection)
 from .spikesorting_artifact import (ArtifactDetection,
                                     ArtifactDetectionParameters,
                                     ArtifactDetectionSelection,
                                     ArtifactRemovedIntervalList)
-from .spikesorting_curation import (AutomaticCurationParameters,
-                                    AutomaticCurationSelection, AutomaticCuration, Curation,
+from .spikesorting_curation import (AutomaticCuration,
+                                    AutomaticCurationParameters,
+                                    AutomaticCurationSelection, Curation,
+                                    FinalizedSpikeSorting,
+                                    FinalizedSpikeSortingSelection,
                                     MetricParameters, MetricSelection,
                                     QualityMetrics, WaveformParameters,
-                                    Waveforms, WaveformSelection, FinalizedSpikeSortingSelection,
-                                    FinalizedSpikeSorting)
+                                    Waveforms, WaveformSelection)
