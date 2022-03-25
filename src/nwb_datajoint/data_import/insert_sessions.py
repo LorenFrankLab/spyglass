f0bc8346b4645914ec1f75df34929454d0edd74f
Eric Denovellis <edeno@bu.edu>
2022-03-25 11:30:17 -0700

    Attempt to fix circular imports

--- src/nwb_datajoint/data_import/insert_sessions.py
+++ src/nwb_datajoint/data_import/insert_sessions.py
@@ -4,7 +4,8 @@ import warnings
 
 import pynwb
 
-from ..common import Nwbfile, get_raw_eseries, populate_all_common
+from .common.common_nwbfile import Nwbfile
+from .common.nwb_helper_fn import get_raw_eseries, populate_all_common
 from .storage_dirs import check_env
 
 
