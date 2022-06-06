a5b4714c67a812943e21b7dc7c56d66b034f07f4
Loren <loren@phy.ucsf.edu>
2022-06-05 18:00:25 -0700

    added code to use multiple channels

--- src/spyglass/share/share_kachery.py
+++ src/spyglass/share/share_kachery.py
@@ -31,15 +31,32 @@ class KacheryChannel(dj.Manual):
 @schema
 class NwbfileKachery(dj.Computed):
     definition = """
+    -> KacheryChannel
     -> Nwbfile
     ---
     nwb_file_uri: varchar(200)  # the uri the NWB file for kachery
     """
 
     def make(self, key):
+        # set up to use the selected kachery channel
+        try:
+            default_kachery_host = os.environ["KACHERY_DAEMON_HOST"]
+            default_kachery_port = os.environ["KACHERY_DAEMON_PORT"]
+        except:
+            default_kachery_host = None
+            default_kachery_port = None
+        
+        kachery_host, kachery_port = (KacheryChannel & {'channel_name' : key['channel_name']}).fetch1('host', 'port')
+        os.environ["KACHERY_DAEMON_HOST"] = kachery_host
+        os.environ["KACHERY_DAEMON_PORT"] = kachery_port
         print(f'Linking {key["nwb_file_name"]} and storing in kachery...')
         key['nwb_file_uri'] = kc.link_file(Nwbfile().get_abs_path(key['nwb_file_name']))
         self.insert1(key)
+        # reset the environment variables if they were set
+        if default_kachery_host is not None and default_kachery_port is not None:
+            os.environ["KACHERY_DAEMON_HOST"] = default_kachery_host
+            os.environ["KACHERY_DAEMON_PORT"] = default_kachery_port
+        
 
 
 @schema
@@ -49,8 +66,24 @@ class AnalysisNwbfileKachery(dj.Computed):
     ---
     analysis_file_uri: varchar(200)  # the uri of the file
     """
-
     def make(self, key):
+        # set up to use the selected kachery channel
+        try:
+            default_kachery_host = os.environ["KACHERY_DAEMON_HOST"]
+            default_kachery_port = os.environ["KACHERY_DAEMON_PORT"]
+        except:
+            default_kachery_host = None
+            default_kachery_port = None
+        
+        kachery_host, kachery_port = (KacheryChannel & {'channel_name' : key['channel_name']}).fetch1('host', 'port')
+        os.environ["KACHERY_DAEMON_HOST"] = kachery_host
+        os.environ["KACHERY_DAEMON_PORT"] = kachery_port
         print(f'Linking {key["analysis_file_name"]} and storing in kachery...')
         key['analysis_file_uri'] = kc.link_file(AnalysisNwbfile().get_abs_path(key['analysis_file_name']))
         self.insert1(key)
+        # reset the environment variables if they were set
+        if default_kachery_host is not None and default_kachery_port is not None:
+            os.environ["KACHERY_DAEMON_HOST"] = default_kachery_host
+            os.environ["KACHERY_DAEMON_PORT"] = default_kachery_port
+        
+ 
