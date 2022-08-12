8f30078dd45fd4fd5ed8dddf01121cf61cdb3cb9
Ryan Ly <rly@lbl.gov>
2022-08-11 18:11:13 -0700

    Add up to ElectrodeGroup

--- src/spyglass/common/common_ephys.py
+++ src/spyglass/common/common_ephys.py
@@ -13,7 +13,7 @@ from .common_region import BrainRegion  # noqa: F401
 from .common_session import Session  # noqa: F401
 from .dj_helper_fn import fetch_nwb  # dj_replace
 from .nwb_helper_fn import (estimate_sampling_rate, get_data_interface,
-                            get_electrode_indices, get_nwb_file,
+                            get_electrode_indices, get_nwb_file, get_config,
                             get_valid_intervals, invalid_electrode_index)
 
 schema = dj.schema('common_ephys')
@@ -32,24 +32,54 @@ class ElectrodeGroup(dj.Imported):
     target_hemisphere = "Unknown": enum("Right", "Left", "Unknown")
     """
 
-    def make(self, key):
+    def make(self, key: dict):
         nwb_file_name = key['nwb_file_name']
         nwb_file_abspath = Nwbfile.get_abs_path(nwb_file_name)
         nwbf = get_nwb_file(nwb_file_abspath)
-        for electrode_group in nwbf.electrode_groups.values():
-            key['electrode_group_name'] = electrode_group.name
-            # add electrode group location if it does not exist, and fetch the row
-            key['region_id'] = BrainRegion.fetch_add(region_name=electrode_group.location)
-            if isinstance(electrode_group.device, ndx_franklab_novela.Probe):
-                key['probe_type'] = electrode_group.device.probe_type
-            key['description'] = electrode_group.description
-            if isinstance(electrode_group, ndx_franklab_novela.NwbElectrodeGroup):
-                # Define target_hemisphere based on targeted x coordinate
-                if electrode_group.targeted_x >= 0:  # if positive or zero x coordinate
-                    key["target_hemisphere"] = "Right"  # define target location as right hemisphere
-                else:  # if negative x coordinate
-                    key["target_hemisphere"] = "Left"  # define target location as left hemisphere
-            self.insert1(key, skip_duplicates=True)
+        config = get_config(nwb_file_abspath)
+
+        # make a dict of device name to PyNWB device object for all devices in the NWB file that are
+        # of type ndx_franklab_novela.DataAcqDevice and thus have the required metadata
+        nwb_egs = {electrode_group.name: electrode_group for electrode_group in nwbf.electrode_groups.values()}
+
+        # make a dict of device name to dict of device metadata from the config YAML if exists
+        if "ElectrodeGroup" in config:
+            config_egs = {eg_dict["electrode_group_name"]: eg_dict for eg_dict in config["ElectrodeGroup"]}
+        else:
+            config_egs = dict()
+
+        all_eg_names = set(nwb_egs.keys()).union(set(config_egs.keys()))
+
+        for eg_name in all_eg_names:
+            new_eg_dict = dict(key)
+
+            if eg_name in nwb_egs:
+                nwb_eg_obj = nwb_egs[eg_name]
+
+                new_eg_dict['electrode_group_name'] = nwb_eg_obj.name
+                # add electrode group location if it does not exist, and fetch the row
+                new_eg_dict['region_id'] = BrainRegion.fetch_add(region_name=nwb_eg_obj.location)
+                if isinstance(nwb_eg_obj.device, ndx_franklab_novela.Probe):
+                    new_eg_dict['probe_type'] = nwb_eg_obj.device.probe_type
+                new_eg_dict['description'] = nwb_eg_obj.description
+                if isinstance(nwb_eg_obj, ndx_franklab_novela.NwbElectrodeGroup):
+                    # Define target_hemisphere based on targeted x coordinate
+                    if nwb_eg_obj.targeted_x >= 0:  # if positive or zero x coordinate
+                        new_eg_dict["target_hemisphere"] = "Right"  # define target location as right hemisphere
+                    else:  # if negative x coordinate
+                        new_eg_dict["target_hemisphere"] = "Left"  # define target location as left hemisphere
+
+            if eg_name in config_egs:  # override new_device_dict with values from config if specified
+                eg = config_egs[eg_name]
+                # NOTE if the yaml dict for ElectrodeGroup includes a probe_type that is not in the Probe table,
+                # the insert will fail
+                new_eg_dict.update(eg)
+                assert {"probe_type": new_eg_dict["probe_type"]} in Probe(), \
+                       (f'Probe type {new_eg_dict["probe_type"]} for '
+                        'Electrode Group "{eg_name}" not found in Probe table.')
+
+            print(new_eg_dict)
+            self.insert1(new_eg_dict, skip_duplicates=True)
 
 
 @schema
