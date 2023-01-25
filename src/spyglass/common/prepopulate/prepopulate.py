556920492a9d86ded6b4c2a2eec7523009131d43
rly <rly@lbl.gov>
2023-01-25 15:11:23 -0800

    Update prepop script and example

--- src/spyglass/common/prepopulate/prepopulate.py
+++ src/spyglass/common/prepopulate/prepopulate.py
@@ -1,3 +1,4 @@
+import datajoint as dj
 import logging
 import os
 import pathlib
@@ -28,18 +29,37 @@ def populate_from_yaml(yaml_path: str):
         d = yaml.safe_load(stream)
 
     for table_name, table_entries in d.items():
-        cls = getattr(sys.modules["spyglass.common"], table_name)
+        table_cls = _get_table_cls(table_name)
         for entry_dict in table_entries:
             # test whether an entity with the primary key(s) already exists in the table
-            primary_key_values = {
-                k: v for k, v in entry_dict.items() if k in cls.primary_key
-            }
-            if primary_key_values not in cls.fetch(*cls.primary_key, as_dict=True):
-                print(
-                    f"Populate: Populating table {cls.__name__} with data {entry_dict}"
+            if not issubclass(table_cls, (dj.Manual, dj.Lookup, dj.Part)):
+                raise ValueError(
+                    f"Prepopulate YAML ('{yaml_path}') contains table '{table_name}' that cannot be "
+                    "prepopulated. Only Manual and Lookup tables can be prepopulated."
                 )
-                cls.insert1(entry_dict)
+            primary_key_values = {
+                k: v for k, v in entry_dict.items() if k in table_cls.primary_key
+            }
+            if primary_key_values not in table_cls.fetch(
+                *table_cls.primary_key, as_dict=True
+            ):
+                print(
+                    f"Populate: Populating table {table_cls.__name__} with data {entry_dict}"
+                )
+                table_cls.insert1(entry_dict)
             else:
                 logging.info(
-                    f"Populate: Entry in {cls.__name__} with primary keys {primary_key_values} already exists."
+                    f"Populate: Entry in {table_cls.__name__} with primary keys {primary_key_values} already exists."
                 )
+
+
+def _get_table_cls(table_name):
+    """Get the spyglass.common class associated with a given table name. Also works for part tables one level deep."""
+    if "." in table_name:  # part table
+        master_table_name = table_name[0 : table_name.index(".")]
+        part_table_name = table_name[table_name.index(".") + 1 :]
+        master_table_cls = getattr(sys.modules["spyglass.common"], master_table_name)
+        part_table_cls = getattr(master_table_cls, part_table_name)
+        return part_table_cls
+    else:
+        return getattr(sys.modules["spyglass.common"], table_name)
