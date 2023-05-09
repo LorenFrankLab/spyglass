f75a4b79614d9e276da7e53f752dccdc89f1cfec
Eric Denovellis <edenovellis@gmail.com>
2023-05-09 11:12:46 -0700

    Add tests

--- tests/common/test_common_interval.py
+++ tests/common/test_common_interval.py
@@ -1,5 +1,8 @@
 import numpy as np
-from spyglass.common.common_interval import interval_list_intersect
+from spyglass.common.common_interval import (
+    interval_list_intersect,
+    interval_set_difference_inds,
+)
 
 
 def test_interval_list_intersect1():
@@ -15,3 +18,58 @@ def test_interval_list_intersect2():
     interval_list2 = np.array([[11, 14]])
     intersection_list = interval_list_intersect(interval_list1, interval_list2)
     assert len(intersection_list) == 0
+
+
+def test_interval_set_difference_inds_no_overlap():
+    intervals1 = [(0, 5), (8, 10)]
+    intervals2 = [(5, 8)]
+    result = interval_set_difference_inds(intervals1, intervals2)
+    assert result == [(0, 5), (8, 10)]
+
+
+def test_interval_set_difference_inds_overlap():
+    intervals1 = [(0, 5), (8, 10)]
+    intervals2 = [(1, 2), (3, 4), (6, 9)]
+    result = interval_set_difference_inds(intervals1, intervals2)
+    assert result == [(0, 1), (4, 5), (9, 10)]
+
+
+def test_interval_set_difference_inds_empty_intervals1():
+    intervals1 = []
+    intervals2 = [(1, 2), (3, 4), (6, 9)]
+    result = interval_set_difference_inds(intervals1, intervals2)
+    assert result == []
+
+
+def test_interval_set_difference_inds_empty_intervals2():
+    intervals1 = [(0, 5), (8, 10)]
+    intervals2 = []
+    result = interval_set_difference_inds(intervals1, intervals2)
+    assert result == [(0, 5), (8, 10)]
+
+
+def test_interval_set_difference_inds_equal_intervals():
+    intervals1 = [(0, 5), (8, 10)]
+    intervals2 = [(0, 5), (8, 10)]
+    result = interval_set_difference_inds(intervals1, intervals2)
+    assert result == []
+
+
+def test_interval_set_difference_inds_multiple_overlaps():
+    intervals1 = [(0, 10)]
+    intervals2 = [(1, 3), (4, 6), (7, 9)]
+    result = interval_set_difference_inds(intervals1, intervals2)
+    assert result == [(0, 1), (3, 4), (6, 7), (9, 10)]
+
+
+# @pytest.mark.parametrize(
+#     "intervals1, intervals2, expected",
+#     [
+#         ([(0, 10)], [(0, 10)], []),
+#         ([(0, 5)], [(3, 7)], [(0, 3)]),
+#         ([(2, 8)], [(0, 4), (6, 10)], [(4, 6)]),
+#     ],
+# )
+# def test_interval_set_difference_inds_parametrized(intervals1, intervals2, expected):
+#     result = interval_set_difference_inds(intervals1, intervals2)
+#     assert result == expected
