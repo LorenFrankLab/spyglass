cc22ee181570e4429e7a0cb7df0aa898316c7002
Eric Denovellis <edeno@bu.edu>
2023-05-12 14:53:45 -0700

    Move merge table outside v1

---
+++ src/spyglass/lfp/lfp_merge.py
@@ -0,0 +1,49 @@
+import datajoint as dj
+from spyglass.lfp.v1 import LFP, ImportedLFP
+
+schema = dj.schema("lfp")
+
+
+@schema
+class LFPOutput(dj.Manual):
+    definition = """
+    lfp_id: uuid
+    """
+
+    class LFP(dj.Part):
+        definition = """
+        -> master
+        -> LFP
+        """
+
+    class ImportedLFP(dj.Part):
+        definition = """
+        -> master
+        -> ImportedLFP
+        """
+
+    @staticmethod
+    def get_lfp_object(key: dict):
+        """Returns the lfp object corresponding to the key
+
+        Parameters
+        ----------
+        key : dict
+            A dictionary containing some combination of
+                                    uuid,
+                                    nwb_file_name,
+                                    lfp_electrode_group_name,
+                                    interval_list_name,
+                                    fir_filter_name
+
+        Returns
+        -------
+        lfp_object
+            The entry or entries in the LFPOutput part table that corresponds to the key
+        """
+        # first check if this returns anything from the LFP table
+        lfp_object = LFPOutput.LFP & key
+        if lfp_object is not None:
+            return LFP & lfp_object.fetch("KEY")
+        else:
+            return ImportedLFP & (LFPOutput.ImportedLFP & key).fetch("KEY")
