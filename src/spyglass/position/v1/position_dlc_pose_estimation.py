037a65de81b191370ad3266f6a634d1a913f03e8
dpeg22 <dpeg22@gmail.com>
2023-05-19 10:10:31 -0700

    fix RawPosition key and fetch_dataframe

--- src/spyglass/position/v1/position_dlc_pose_estimation.py
+++ src/spyglass/position/v1/position_dlc_pose_estimation.py
@@ -230,7 +230,9 @@ class DLCPoseEstimation(dj.Computed):
 
             logger.logger.info("getting raw position")
             interval_list_name = f"pos {key['epoch']-1} valid times"
-            spatial_series = (RawPosition() & key).fetch_nwb()[0]["raw_position"]
+            spatial_series = (
+                RawPosition() & {**key, "interval_list_name": interval_list_name}
+            ).fetch_nwb()[0]["raw_position"]
             _, _, _, video_time = get_video_path(key)
             pos_time = spatial_series.timestamps
             # TODO: should get timestamps from VideoFile, but need the video_frame_ind from RawPosition,
@@ -317,11 +319,47 @@ class DLCPoseEstimation(dj.Computed):
 
     def fetch_dataframe(self, *attrs, **kwargs):
         entries = (self.BodyPart & self).fetch("KEY")
+
+        nwb_data = self.fetch_nwb()[0]
+        index = pd.Index(
+            np.asarray(
+                nwb_data["dlc_pose_estimation_position"].get_spatial_series().timestamps
+            ),
+            name="time",
+        )
+        COLUMNS = [
+            "video_frame_ind",
+            "x",
+            "y",
+            "likelihood",
+        ]
         return pd.concat(
             {
-                entry["bodypart"]: (self.BodyPart & entry).fetch_nwb()[0][
-                    "dlc_pose_estimation"
-                ]
+                entry["bodypart"]: pd.DataFrame(
+                    np.concatenate(
+                        (
+                            np.asarray(
+                                nwb_data["dlc_pose_estimation_likelihood"]
+                                .time_series["video_frame_ind"]
+                                .data,
+                                dtype=int,
+                            )[:, np.newaxis],
+                            np.asarray(
+                                nwb_data["dlc_pose_estimation_position"]
+                                .get_spatial_series()
+                                .data
+                            ),
+                            np.asarray(
+                                nwb_data["dlc_pose_estimation_likelihood"]
+                                .time_series["likelihood"]
+                                .data
+                            )[:, np.newaxis],
+                        ),
+                        axis=1,
+                    ),
+                    columns=COLUMNS,
+                    index=index,
+                )
                 for entry in entries
             },
             axis=1,
