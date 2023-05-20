12a35fb68928cfbcfc29d8663fb6ef18a04e1520
Shijie Gu <xlsun79@gmail.com>
2023-05-19 20:03:53 -0700

    notebook fixes as suggested on git pull request

--- src/spyglass/lfp/v1/analytic_signal.py
+++ src/spyglass/lfp/v1/analytic_signal.py
@@ -25,7 +25,7 @@ class AnalyticSignalParameters(dj.Manual):
         analytic_signal_params["analytic_method_name"] = "hilbert_transform"
         analytic_signal_params[
             "other_params"
-        ] = None # Hilbert transform doesn't contain other parameters; alternative methods may contain additional params.
+        ] = None  # Hilbert transform doesn't contain other parameters; alternative methods may contain additional params.
         self.insert1(["default", analytic_signal_params], skip_duplicates=True)
 
 
