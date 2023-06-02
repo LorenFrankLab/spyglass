3865ee67d7b508951eb5fc45a89db9f7370f0849
Eric Denovellis <edenovellis@gmail.com>
2023-06-02 16:33:02 -0700

    Revert "Remove get_lfp_object method"
    This reverts commit cfd05459651fb70a9f3169ddec0a3a995bb4d9e5.

--- src/spyglass/lfp/lfp_merge.py
+++ src/spyglass/lfp/lfp_merge.py
@@ -97,3 +97,29 @@ class LFPOutput(dj.Manual):
                 nwb_lfp["lfp"].data,
                 index=pd.Index(nwb_lfp["lfp"].timestamps, name="time"),
             )
+
+    @staticmethod
+    def get_lfp_object(key: dict):
+        """Returns the lfp object corresponding to the key
+
+        Parameters
+        ----------
+        key : dict
+            A dictionary containing some combination of
+                                    uuid,
+                                    nwb_file_name,
+                                    lfp_electrode_group_name,
+                                    interval_list_name,
+                                    fir_filter_name
+
+        Returns
+        -------
+        lfp_object
+            The entry or entries in the LFPOutput part table that corresponds to the key
+        """
+        # first check if this returns anything from the LFP table
+        lfp_object = LFPOutput.LFP & key
+        if lfp_object is not None:
+            return LFP & lfp_object.fetch("KEY")
+        else:
+            return ImportedLFP & (LFPOutput.ImportedLFP & key).fetch("KEY")
