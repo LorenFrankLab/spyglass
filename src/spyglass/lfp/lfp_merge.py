7a5d21cf2937199fef08284289e9715e3987c718
Eric Denovellis <edenovellis@gmail.com>
2023-06-22 13:42:44 -0700

    Remove unused method

--- src/spyglass/lfp/lfp_merge.py
+++ src/spyglass/lfp/lfp_merge.py
@@ -5,7 +5,7 @@ from spyglass.common.common_ephys import LFP as CommonLFP  # noqa: F401
 from spyglass.common.common_filter import FirFilterParameters  # noqa: F401
 from spyglass.common.common_interval import IntervalList  # noqa: F401
 from spyglass.common.common_nwbfile import AnalysisNwbfile
-from spyglass.lfp.v1.lfp import LFPV1, ImportedLFPV1
+from spyglass.lfp.v1.lfp import LFPV1, ImportedLFPV1  # noqa: F401
 from spyglass.utils.dj_helper_fn import fetch_nwb
 from spyglass.utils.dj_merge_tables import Merge
 
@@ -42,32 +42,6 @@ class LFPOutput(Merge):
         ---
         -> CommonLFP
         """
-
-    @staticmethod
-    def get_lfp_object(key: dict):
-        """Returns the lfp object corresponding to the key
-
-        Parameters
-        ----------
-        key : dict
-            A dictionary containing some combination of
-                                    uuid,
-                                    nwb_file_name,
-                                    lfp_electrode_group_name,
-                                    interval_list_name,
-                                    fir_filter_name
-
-        Returns
-        -------
-        lfp_object
-            The entry or entries in the LFPOutput part table that corresponds to the key
-        """
-        # first check if this returns anything from the LFP table
-        lfp_object = LFPOutput.LFP & key
-        if lfp_object is not None:
-            return LFPV1 & lfp_object.fetch("KEY")
-        else:
-            return ImportedLFPV1 & (LFPOutput.ImportedLFP & key).fetch("KEY")
 
     def fetch_nwb(self, *attrs, **kwargs):
         part_parents = self._merge_restrict_parents(
