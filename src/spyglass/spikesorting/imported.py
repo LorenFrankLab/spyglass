c2adb1daa09d97ce4a76efd403ad8a076b7d8841
Kyu Hyun Lee <kyuhyun9056@gmail.com>
2023-07-22 17:44:39 -0700

    Add new tables

---
+++ src/spyglass/spikesorting/imported.py
@@ -0,0 +1,12 @@
+import datajoint as dj
+from spyglass.common.common_session import Session
+
+schema = dj.schema("spikesorting_imported")
+
+@schema
+class ImportedSpikeSorting(dj.Imported):
+    definition = """
+    -> Session
+    ---
+    object_id: varchar(32)
+    """
---
+++ src/spyglass/spikesorting/merge.py
@@ -0,0 +1,66 @@
+import datajoint as dj
+import pandas as pd
+
+from spyglass.common.common_ephys import LFP as CommonLFP  # noqa: F401
+from spyglass.common.common_filter import FirFilterParameters  # noqa: F401
+from spyglass.common.common_interval import IntervalList  # noqa: F401
+from spyglass.spikesorting.v1.artifact import ArtifactIntervalV1  # noqa: F401
+from spyglass.utils.dj_merge_tables import _Merge
+
+schema = dj.schema("spikesorting_merge")
+
+
+
+@schema
+class ArtifactOutput(_Merge):
+    definition = """
+    merge_id: uuid
+    ---
+    source: varchar(32)
+    """
+
+    class ArtifactIntervalV1(dj.Part):
+        definition = """
+        -> master
+        ---
+        -> ArtifactIntervalV1
+        """
+
+@schema
+class SpikeSortingOutput(_Merge):
+    definition = """
+    merge_id: uuid
+    ---
+    source: varchar(32)
+    """
+
+    class SpikeSortingV1(dj.Part):
+        definition = """
+        -> master
+        ---
+        -> LFPV1
+        """
+
+    class ImportedSpikeSorting(dj.Part):
+        definition = """
+        -> master
+        ---
+        -> ImportedLFPV1
+        """
+
+    class CommonLFP(dj.Part):
+        """Table to pass-through legacy LFP"""
+
+        definition = """
+        -> master
+        ---
+        -> CommonLFP
+        """
+
+    def fetch1_dataframe(self, *attrs, **kwargs):
+        # Note: `proj` below facilitates operator syntax eg Table & restrict
+        nwb_lfp = self.fetch_nwb(self.proj())[0]
+        return pd.DataFrame(
+            nwb_lfp["lfp"].data,
+            index=pd.Index(nwb_lfp["lfp"].timestamps, name="time"),
+        )
