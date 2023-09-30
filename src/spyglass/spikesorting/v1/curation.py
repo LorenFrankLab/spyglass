9adb9044fa8dd861556eac3cdbc9d3b5188469e0
Kyu Hyun Lee <kyuhyun9056@gmail.com>
2023-09-29 17:02:15 -0700

    Write sorting with curation

--- src/spyglass/spikesorting/v1/curation.py
+++ src/spyglass/spikesorting/v1/curation.py
@@ -35,6 +35,7 @@ class Curation(dj.Manual):
     parent_curation_id=-1: int
     -> AnalysisNwbfile
     object_id: varchar(72)
+    description: varchar(100)
     """
 
     @staticmethod
@@ -49,7 +50,6 @@ class Curation(dj.Manual):
         """Given a sorting_id and the parent_sorting_id (and optional
         arguments) insert an entry into Curation.
 
-
         Parameters
         ----------
         sorting_id : str
@@ -57,32 +57,27 @@ class Curation(dj.Manual):
         parent_curation_id : int, optional
             The id of the parent sorting
         labels : dict or None, optional
+            curation labels (e.g. good, noise, mua)
         merge_groups : dict or None, optional
+            groups of unit IDs to be merged
         metrics : dict or None, optional
-            Computed metrics for sorting
+            Computed quality metrics, one for each neuron
         description : str, optional
-            text description of this sort
+            description of this curation or where it originates; e.g. "FigURL", by default ""
 
         Returns
         -------
         curation_key : dict
-
         """
         if parent_curation_id <= -1:
             parent_curation_id = -1
             # check to see if this sorting with a parent of -1 has already been inserted and if so, warn the user
-            if len((Curation & [sorting_id, parent_curation_id]).fetch("KEY")) > 0:
-                Warning(
-                    f"Sorting has already been inserted."
-                )
+            if (
+                len((Curation & [sorting_id, parent_curation_id]).fetch("KEY"))
+                > 0
+            ):
+                Warning(f"Sorting has already been inserted.")
                 return inserted_curation[0]
-
-        if labels is None:
-            labels = {}
-        if merge_groups is None:
-            merge_groups = []
-        if metrics is None:
-            metrics = {}
 
         # generate a unique number for this curation
         existing_curation_ids = (Curation & sorting_id).fetch("curation_id")
@@ -91,26 +86,37 @@ class Curation(dj.Manual):
         else:
             curation_id = 0
 
-        # convert unit_ids in labels to integers for labels from sortingview.
-        new_labels = {int(unit_id): labels[unit_id] for unit_id in labels}
+        if labels is None:
+            labels = {}
+        else:
+            labels = {int(unit_id): labels[unit_id] for unit_id in labels}
+        if merge_groups is None:
+            merge_groups = {}
+        if metrics is None:
+            metrics = {}
 
-        # TODO: write the curation labels, merge groups, and metrics as columns in the units table of NWB
-
-
-        # mike: added skip duplicates
-        Curation.insert1(
-            [sorting_id, curation_id, parent_curation_id, analysis_file_name],
-            skip_duplicates=True,
+        # write the curation labels, merge groups, and metrics as columns in the units table of NWB
+        analysis_file_name, object_id = _write_sorting_to_nwb_with_curation(
+            sorting_id=sorting_id,
+            labels=labels,
+            merge_groups=merge_groups,
+            metrics=metrics,
         )
+        # INSERT
+        key = {
+            "sorting_id": sorting_id,
+            "curation_id": curation_id,
+            "parent_curation_id": parent_curation_id,
+            "analysis_file_name": analysis_file_name,
+            "object_id": object_id,
+            "description": description,
+        }
+        Curation.insert1(key, skip_duplicates=True)
 
-        # get the primary key for this curation
-        c_key = Curation.fetch("KEY")[0]
-        curation_key = {item: sorting_key[item] for item in c_key}
-
-        return curation_key
+        return key
 
     @staticmethod
-    def get_recording(key: dict)->si.BaseRecording:
+    def get_recording(key: dict) -> si.BaseRecording:
         """Get recording related to this curation
 
         Parameters
@@ -119,8 +125,10 @@ class Curation(dj.Manual):
             primary key of Curation table
         """
 
-        recording_id = (SpikeSorting & key).fetch1('recording_id')
-        analysis_file_name = (SpikeSortingRecording & {"recording_id": recording_id}).fetch1("analysis_file_name")
+        recording_id = (SpikeSorting & key).fetch1("recording_id")
+        analysis_file_name = (
+            SpikeSortingRecording & {"recording_id": recording_id}
+        ).fetch1("analysis_file_name")
         analysis_file_abs_path = AnalysisNwbfile.get_abs_path(
             analysis_file_name
         )
@@ -131,8 +139,8 @@ class Curation(dj.Manual):
         return recording
 
     @staticmethod
-    def get_sorting(key: dict)->si.BaseSorting:
-        """Get sorting related to the curation
+    def get_original_sorting(key: dict) -> si.BaseSorting:
+        """Get sorting prior to the curation
 
         Parameters
         ----------
@@ -140,18 +148,16 @@ class Curation(dj.Manual):
             primary key of Curation table
         """
 
-        analysis_file_name = (SpikeSorting & key).fetch1('analysis_file_name')
+        analysis_file_name = (SpikeSorting & key).fetch1("analysis_file_name")
         analysis_file_abs_path = AnalysisNwbfile.get_abs_path(
             analysis_file_name
         )
-        recording = se.read_nwb_sorting(
-            analysis_file_abs_path
-        )
+        sorting = se.read_nwb_sorting(analysis_file_abs_path)
 
-        return recording
+        return sorting
 
     @staticmethod
-    def get_merged_sorting(key: dict)->si.BaseSorting:
+    def get_merged_sorting(key: dict) -> si.BaseSorting:
         """Returns the sorting extractor related to this curation,
         with merges applied.
 
@@ -170,14 +176,17 @@ class Curation(dj.Manual):
         analysis_file_abs_path = AnalysisNwbfile.get_abs_path(
             analysis_file_name
         )
-
         object_id = (Curation & key).fetch1("object_id")
-        with pynwb.NWBHDF5IO(analysis_file_abs_path, 'r', load_namespaces=True) as io:
+        with pynwb.NWBHDF5IO(
+            analysis_file_abs_path, "r", load_namespaces=True
+        ) as io:
             nwbfile = io.read()
             nwb_sorting = nwbfile.objects[object_id]
-        sorting = MergeUnitsSorting(si_sorting, units_to_merge)
-        if len(nwb_sorting["merge_groups"]) != 0:
-            units_to_merge = _process(nwb_sorting["merge_groups"])
+            merge_groups = nwb_sorting["merge_groups"][:]
+        # TODO: process merge groups which is dict of int to list of int
+        units_to_merge = _process(nwb_sorting["merge_groups"])
+        sorting = si.MergeUnitsSorting(si_sorting, units_to_merge)
+        if units_to_merge:
             return si.MergeUnitsSorting(
                 parent_sorting=si_sorting, units_to_merge=units_to_merge
             )
@@ -186,27 +195,38 @@ class Curation(dj.Manual):
 
 
 def _write_sorting_to_nwb_with_curation(
-    sorting: si.BaseSorting,
-    nwb_file_name: str,
-    labels=None,
-    metrics=None,
-    unit_ids=None,
+    sorting_id: str,
+    labels: Union[None, Dict[int, List[str]]] = None,
+    merge_groups: Union[None, Dict[int, List[int]]] = None,
+    metrics: Union[None, Dict[str, Dict[int, float]]] = None,
 ):
-    """Write a recording in NWB format
+    """Loads the previously saved NWB file containing sorting, adds new information
+    for curation (e.g. labels, merge groups, metrics) and saves the new NWB file.
 
     Parameters
     ----------
-    sorting : si.BaseSorting
-    sort_interval : Iterable
-    nwb_file_name : str
-        name of NWB file the recording originates
+    original_nwb_file : str
+        name of original NWB file containing the recording
 
     Returns
     -------
     analysis_nwb_file : str
-        name of analysis NWB file containing the sorting
+        name of new analysis NWB file containing the sorting and other curation information
+    object_id : str
+        object_id of the sorting in the new analysis NWB file
     """
+    # get original nwb file
+    sorting_key = (SpikeSorting & {"sorting_id": sorting_id}).fetch1()
+    recording_key = (SpikeSortingRecording & sorting_key).fetch1()
+    nwb_file_name = recording_key["nwb_file_name"]
 
+    # get sorting
+    sorting_analysis_file_abs_path = AnalysisNwbfile.get_abs_path(
+        sorting_key["analysis_file_name"]
+    )
+    sorting = se.read_nwb_sorting(sorting_analysis_file_abs_path)
+
+    # create new analysis nwb file
     analysis_nwb_file = AnalysisNwbfile().create(nwb_file_name)
     analysis_nwb_file_abs_path = AnalysisNwbfile.get_abs_path(analysis_nwb_file)
     with pynwb.NWBHDF5IO(
@@ -215,100 +235,49 @@ def _write_sorting_to_nwb_with_curation(
         load_namespaces=True,
     ) as io:
         nwbf = io.read()
-        nwbf.add_unit_column(
-            name="quality",
-            description="quality of unit",
-        )
+        # add labels, merge groups, metrics
+        if labels is not None:
+            for unit in unit_ids:
+                if unit not in labels:
+                    labels[unit] = ""
+            label_values = [labels[key] for key in sorted(labels.keys())]
+            nwbf.add_unit_column(
+                name="curation_label",
+                description="curation label",
+                data=label_values,
+            )
+        if merge_groups is not None:
+            for unit in unit_ids:
+                if unit not in labels:
+                    merge_groups[unit] = []
+            merge_groups_values = [
+                merge_groups[key] for key in sorted(merge_groups.keys())
+            ]
+            nwbf.add_unit_column(
+                name="merge_groups",
+                description="merge groups",
+                data=merge_groups_values,
+            )
+        if metrics is not None:
+            if metrics[metric]:
+                for metric in metrics:
+                    metric_values = [
+                        metrics[metric][key]
+                        for key in sorted(metrics[metric].keys())
+                    ]
+                    nwbf.add_unit_column(
+                        name=metric,
+                        description=metric,
+                        data=metric_values,
+                    )
+
+        # write sorting to the nwb file
         for unit_id in sorting.get_unit_ids():
             spike_times = sorting.get_unit_spike_train(unit_id)
             nwbf.add_unit(
                 spike_times=spike_times,
                 id=unit_id,
-                obs_intervals=sort_interval,
-                quality="uncurated",
             )
         units_object_id = nwbf.units.object_id
         io.write(nwbf)
     return analysis_nwb_file, units_object_id
-
-
-
-def _save_sorting_nwb(
-    key,
-    sorting,
-    timestamps,
-    sort_interval_list_name,
-    sort_interval,
-    labels=None,
-    metrics=None,
-    unit_ids=None,
-):
-    """Store a sorting in a new AnalysisNwbfile
-
-    Parameters
-    ----------
-    key : dict
-        key to SpikeSorting table
-    sorting : si.Sorting
-        sorting
-    timestamps : array_like
-        Time stamps of the sorted recoridng;
-        used to convert the spike timings from index to real time
-    sort_interval_list_name : str
-        name of sort interval
-    sort_interval : list
-        interval for start and end of sort
-    labels : dict, optional
-        curation labels, by default None
-    metrics : dict, optional
-        quality metrics, by default None
-    unit_ids : list, optional
-        IDs of units whose spiketrains to save, by default None
-
-    Returns
-    -------
-    analysis_file_name : str
-    units_object_id : str
-
-    """
-
-    sort_interval_valid_times = (
-        IntervalList & {"interval_list_name": sort_interval_list_name}
-    ).fetch1("valid_times")
-
-    units = dict()
-    units_valid_times = dict()
-    units_sort_interval = dict()
-
-    if unit_ids is None:
-        unit_ids = sorting.get_unit_ids()
-
-    for unit_id in unit_ids:
-        spike_times_in_samples = sorting.get_unit_spike_train(
-            unit_id=unit_id
-        )
-        units[unit_id] = timestamps[spike_times_in_samples]
-        units_valid_times[unit_id] = sort_interval_valid_times
-        units_sort_interval[unit_id] = [sort_interval]
-
-    analysis_file_name = AnalysisNwbfile().create(key["nwb_file_name"])
-    object_ids = AnalysisNwbfile().add_units(
-        analysis_file_name,
-        units,
-        units_valid_times,
-        units_sort_interval,
-        metrics=metrics,
-        labels=labels,
-    )
-    AnalysisNwbfile().add(key["nwb_file_name"], analysis_file_name)
-
-    if object_ids == "":
-        print(
-            "Sorting contains no units."
-            "Created an empty analysis nwb file anyway."
-        )
-        units_object_id = ""
-    else:
-        units_object_id = object_ids[0]
-
-    return analysis_file_name, units_object_id
