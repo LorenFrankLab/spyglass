f70f2b091b61dba2bffd36248230abd76e94fcdf
Kyu Hyun Lee <kyuhyun9056@gmail.com>
2023-10-10 16:01:09 -0700

    Update init

--- src/spyglass/spikesorting/v1/__init__.py
+++ src/spyglass/spikesorting/v1/__init__.py
@@ -4,5 +4,18 @@ from .recording import (
     SpikeSortingRecordingSelection,
     SpikeSortingRecording,
 )
-
-from .sorting import SpikeSorterParameter, SpikeSortingSelection, SpikeSortingV1
+from .artifact import (
+    ArtifactDetectionParameter,
+    ArtifactDetectionSelection,
+    ArtifactRemovedInterval,
+)
+from .sorting import SpikeSorterParameter, SpikeSortingSelection, SpikeSorting
+from .curation import CurationV1
+from .metric_curation import (
+    WaveformParameter,
+    MetricParameter,
+    MetricCurationParameter,
+    MetricCurationSelection,
+    MetricCuration,
+)
+from .figurl_curation import FigURLCurationSelection, FigURLCuration
