16d929a27574b2055dc8b38fe84bf0c967c98537
Kyu Hyun Lee <kyuhyun9056@gmail.com>
2023-11-15 16:07:52 -0800

    Update merge

--- src/spyglass/spikesorting/merge.py
+++ src/spyglass/spikesorting/merge.py
@@ -1,5 +1,5 @@
 import datajoint as dj
-
+import uuid
 from spyglass.spikesorting.v1.curation import CurationV1  # noqa: F401
 from spyglass.spikesorting.imported import ImportedSpikeSorting  # noqa: F401
 from spyglass.utils.dj_merge_tables import _Merge
@@ -10,7 +10,7 @@ schema = dj.schema("spikesorting_merge")
 @schema
 class SpikeSortingOutput(_Merge):
     definition = """
-    merge_id: uuid
+    merge_id: varchar(36)
     ---
     source: varchar(32)
     """
@@ -28,3 +28,33 @@ class SpikeSortingOutput(_Merge):
         ---
         -> ImportedSpikeSorting
         """
+
+    @classmethod
+    def insert_from_CurationV1(cls, key):
+        """Insert a row from CurationV1 into SpikeSortingOutput
+
+        Parameters
+        ----------
+        key : dict
+            primary key of CurationV1 table
+        """
+        key["merge_id"] = str(uuid.uuid4())
+        key["source"] = "CurationV1"
+        cls.insert1([key["merge_id"]], skip_duplicates=True)
+        cls.CurationV1.insert1(key, skip_duplicates=True)
+        return key
+
+    @classmethod
+    def insert_from_ImportedSpikeSorting(cls, key):
+        """Insert a row from ImportedSpikeSorting into SpikeSortingOutput
+
+        Parameters
+        ----------
+        key : dict
+            primary key of ImportedSpikeSorting table
+        """
+        key["merge_id"] = str(uuid.uuid4())
+        key["source"] = "ImportedSpikeSorting"
+        cls.insert1([key["merge_id"]], skip_duplicates=True)
+        cls.CurationV1.insert1(key, skip_duplicates=True)
+        return key
