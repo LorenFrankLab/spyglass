27bb5a13df1eeabd5177a59533572de83ba60ce6
emreybroyles <114687400+emreybroyles@users.noreply.github.com>
2023-12-14 15:16:31 -0800

    Dlc debug (#702)
    * add envs to bashrc; multi cam addition
    
    * 12/11/23 using TackEpoch to define interval_names
    
    * 12/11/23 remove comment
    
    * revert notebook 21
    
    * formatting fixes
    
    * formatting fix pt 2

--- src/spyglass/common/common_behav.py
+++ src/spyglass/common/common_behav.py
@@ -596,13 +596,9 @@ def get_interval_list_name_from_epoch(nwb_file_name: str, epoch: int) -> str:
     interval_list_name : str
         The interval list name.
     """
-    interval_names = [
-        x
-        for x in (IntervalList() & {"nwb_file_name": nwb_file_name}).fetch(
-            "interval_list_name"
-        )
-        if (x.split("_")[0] == f"{epoch:02}")
-    ]
+    interval_names = (
+        TaskEpoch & {"nwb_file_name": nwb_file_name, "epoch": epoch}
+    ).fetch("interval_list_name")
 
     if len(interval_names) != 1:
         print(
