de083a6f5d15c120af38bbad5fe61dcbd34693be
Sam Bray <sam.bray@ucsf.edu>
2023-12-13 16:21:10 -0800

    use isclose for comparison check

--- src/spyglass/common/common_filter.py
+++ src/spyglass/common/common_filter.py
@@ -461,7 +461,7 @@ class FirFilterParameters(dj.Manual):
             frm, to = self._time_bound_check(
                 a_start, a_stop, timestamps, n_samples
             )
-            if frm == to:
+            if np.isclose(frm, to, rtol=0, atol=1e-8):
                 continue
             indices.append((frm, to))
 
