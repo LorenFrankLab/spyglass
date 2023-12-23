17b1abb0961bf5bb9176f62d5ed2f7e0c4d1b9ea
Eric Denovellis <edeno@bu.edu>
2023-12-23 14:07:10 -0800

    Add objects to init for convenience

---
+++ src/spyglass/decoding/__init__.py
@@ -0,0 +1,7 @@
+from spyglass.decoding.decoding_merge import DecodingOutput  # noqa: E402
+from spyglass.decoding.visualization.core import (  # noqa: E402
+    create_interactive_1D_decoding_figurl,
+    create_interactive_2D_decoding_figurl,
+    make_multi_environment_movie,
+    make_single_environment_movie,
+)
