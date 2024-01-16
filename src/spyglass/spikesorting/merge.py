fe9cebb2517372ed9084c4db003f15e777f877ed
Eric Denovellis <edenovellis@gmail.com>
2024-01-16 12:23:17 -0800

    Add methods to get the spike_times, spike_indicators, firing rate

--- src/spyglass/spikesorting/merge.py
+++ src/spyglass/spikesorting/merge.py
@@ -1,5 +1,7 @@
 import datajoint as dj
+import numpy as np
 from datajoint.utils import to_camel_case
+from ripple_detection import get_multiunit_population_firing_rate
 
 from spyglass.spikesorting.imported import ImportedSpikeSorting  # noqa: F401
 from spyglass.spikesorting.spikesorting_curation import (  # noqa: F401
@@ -8,6 +10,18 @@ from spyglass.spikesorting.spikesorting_curation import (  # noqa: F401
 from spyglass.spikesorting.v1.curation import CurationV1  # noqa: F401
 from spyglass.utils.dj_merge_tables import _Merge
 from spyglass.utils.dj_mixin import SpyglassMixin
+
+try:
+    from non_local_detector.likelihoods.common import (
+        get_spikecount_per_time_bin,
+    )
+except ImportError:
+
+    def get_spikecount_per_time_bin(*args, **kwargs):
+        raise NotImplementedError(
+            "Please install non_local_detector to use this function"
+        )
+
 
 schema = dj.schema("spikesorting_merge")
 
@@ -48,6 +62,7 @@ class SpikeSortingOutput(_Merge, SpyglassMixin):
         -> CuratedSpikeSorting
         """
 
+    @classmethod
     def get_recording(cls, key):
         """get the recording associated with a spike sorting output"""
         source_table = source_class_dict[
@@ -56,6 +71,7 @@ class SpikeSortingOutput(_Merge, SpyglassMixin):
         query = source_table & cls.merge_get_part(key)
         return query.get_recording(query.fetch("KEY"))
 
+    @classmethod
     def get_sorting(cls, key):
         """get the sorting associated with a spike sorting output"""
         source_table = source_class_dict[
@@ -63,3 +79,52 @@ class SpikeSortingOutput(_Merge, SpyglassMixin):
         ]
         query = source_table & cls.merge_get_part(key)
         return query.get_sorting(query.fetch("KEY"))
+
+    @classmethod
+    def get_spike_times(cls, key):
+        spike_times = []
+        for nwb_file in cls.fetch_nwb(key):
+            if "object_id" in nwb_file:
+                # v1 spikesorting
+                spike_times.extend(
+                    nwb_file["object_id"]["spike_times"].to_list()
+                )
+            elif "units" in nwb_file:
+                # v0 spikesorting
+                spike_times.extend(nwb_file["units"]["spike_times"].to_list())
+        return spike_times
+
+    @classmethod
+    def get_spike_indicator(cls, key, time):
+        min_time, max_time = time[[0, -1]]
+        spike_times = cls.get_spike_times(key)
+        spike_indicator = np.zeros((len(time), len(spike_times)))
+
+        for ind, times in enumerate(spike_times):
+            is_in_time = np.logical_and(times >= min_time, times <= max_time)
+            spike_indicator[:, ind] = get_spikecount_per_time_bin(
+                times[is_in_time],
+                np.asarray(time),
+            )
+
+        return spike_indicator
+
+    @classmethod
+    def get_firing_rate(cls, key, time, multiunit=False):
+        spike_indicator = cls.get_spike_indicator(key, time)
+        if spike_indicator.ndim == 1:
+            spike_indicator = spike_indicator[:, np.newaxis]
+
+        sampling_frequency = 1 / np.median(np.diff(time))
+
+        if multiunit:
+            spike_indicator = spike_indicator.sum(axis=1, keepdims=True)
+        return np.stack(
+            [
+                get_multiunit_population_firing_rate(
+                    indicator[:, np.newaxis], sampling_frequency
+                )
+                for indicator in spike_indicator.T
+            ],
+            axis=1,
+        )
