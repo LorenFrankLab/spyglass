b4e83cdf446b80e7615be53ed2870ed8d1d9eeaa
Eric Denovellis <edenovellis@gmail.com>
2024-01-17 18:06:24 -0800

    Add draft helper functions

--- src/spyglass/decoding/decoding_merge.py
+++ src/spyglass/decoding/decoding_merge.py
@@ -1,7 +1,12 @@
+import inspect
 from itertools import chain
 from pathlib import Path
 
 import datajoint as dj
+import numpy as np
+from datajoint.utils import to_camel_case
+from non_local_detector.visualization.figurl_1D import create_1D_decode_view
+from non_local_detector.visualization.figurl_2D import create_2D_decode_view
 
 from spyglass.decoding.v1.clusterless import ClusterlessDecodingV1  # noqa: F401
 from spyglass.decoding.v1.sorted_spikes import (
@@ -78,3 +83,87 @@ class DecodingOutput(_Merge, SpyglassMixin):
                         path.unlink()
                     except (PermissionError, FileNotFoundError):
                         logger.warning(f"Unable to remove {path}, skipping")
+
+    @classmethod
+    def _get_source_class(cls, key):
+        source_classes = {}
+        module = inspect.getmodule(cls)
+        for part_name in cls.parts():
+            part_name = to_camel_case(part_name.split("__")[-1].strip("`"))
+            part = getattr(module, part_name)
+            source_classes[part_name] = part
+
+        source = (cls & key).fetch1("source")
+        return source_classes[source]
+
+    @classmethod
+    def load_results(cls, key):
+        decoding_selection_key = cls.merge_get_parent(key).fetch1("KEY")
+        source_class = cls._get_source_class(key)
+        return source_class.load_results(decoding_selection_key)
+
+    @classmethod
+    def load_model(cls, key):
+        decoding_selection_key = cls.merge_get_parent(key).fetch1("KEY")
+        source_class = cls._get_source_class(key)
+        return source_class.load_model(decoding_selection_key)
+
+    @classmethod
+    def load_environments(cls, key):
+        decoding_selection_key = cls.merge_get_parent(key).fetch1("KEY")
+        source_class = cls._get_source_class(key)
+        return source_class.load_environments(decoding_selection_key)
+
+    @classmethod
+    def load_position_info(cls, key):
+        decoding_selection_key = cls.merge_get_parent(key).fetch1("KEY")
+        source_class = cls._get_source_class(key)
+        return source_class.load_position_info(decoding_selection_key)
+
+    @classmethod
+    def load_linear_position_info(cls, key):
+        decoding_selection_key = cls.merge_get_parent(key).fetch1("KEY")
+        source_class = cls._get_source_class(key)
+        return source_class.load_linear_position_info(decoding_selection_key)
+
+    @classmethod
+    def load_spike_data(cls, key, filter_by_interval=True):
+        decoding_selection_key = cls.merge_get_parent(key).fetch1("KEY")
+        source_class = cls._get_source_class(key)
+        return source_class.load_linear_position_info(
+            decoding_selection_key, filter_by_interval=filter_by_interval
+        )
+
+    @classmethod
+    def create_decoding_view(cls, key, head_direction_name="head_orientation"):
+        results = cls.load_results(key)
+        posterior = results.acausal_posterior.unstack("state_bins").sum("state")
+        env = cls.load_environments(key)[0]
+
+        if "x_position" in results.coords:
+            position_info, position_variable_names = cls.load_position_info(key)
+            # Not 1D
+            bin_size = (
+                np.nanmedian(np.diff(np.unique(results.x_position.values))),
+                np.nanmedian(np.diff(np.unique(results.y_position.values))),
+            )
+            return create_2D_decode_view(
+                position_time=position_info.index,
+                position=position_info[position_variable_names],
+                interior_place_bin_centers=env.place_bin_centers_[
+                    env.is_track_interior_.ravel(order="C")
+                ],
+                place_bin_size=bin_size,
+                posterior=posterior,
+                head_dir=position_info[head_direction_name],
+            )
+        else:
+            (
+                position_info,
+                position_variable_names,
+            ) = cls.load_linear_position_info(key)
+            return create_1D_decode_view(
+                posterior=posterior,
+                linear_position=position_info["linear_position"],
+                ref_time_sec=position_info.index[0],
+            )
