f9352564e9c16911cab834838989f4149b65a969
Eric Denovellis <edeno@bu.edu>
2024-01-18 14:15:06 -0800

    Add useful imports to the top level
    This would have to change a bit if there were multiple versions of the pipeline.

--- src/spyglass/decoding/__init__.py
+++ src/spyglass/decoding/__init__.py
@@ -1,1 +1,20 @@
 from spyglass.decoding.decoding_merge import DecodingOutput  # noqa: E402
+from spyglass.decoding.v1.clusterless import (  # noqa: E402
+    ClusterlessDecodingSelection,
+    ClusterlessDecodingV1,
+    UnitWaveformFeaturesGroup,
+)
+from spyglass.decoding.v1.core import (
+    DecodingParameters,
+    PositionGroup,
+)  # noqa: E402
+from spyglass.decoding.v1.sorted_spikes import (  # noqa: E402
+    SortedSpikesDecodingSelection,
+    SortedSpikesDecodingV1,
+    SortedSpikesGroup,
+)
+from spyglass.decoding.v1.waveform_features import (  # noqa: E402
+    UnitWaveformFeatures,
+    UnitWaveformFeaturesSelection,
+    WaveformFeaturesParams,
+)
