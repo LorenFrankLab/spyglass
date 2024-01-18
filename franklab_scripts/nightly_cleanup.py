345de1ac9542d816d624fdb6a06584d72562b514
Eric Denovellis <edeno@bu.edu>
2024-01-17 16:30:02 -0800

    Add decoding cleanup

--- franklab_scripts/nightly_cleanup.py
+++ franklab_scripts/nightly_cleanup.py
@@ -13,12 +13,14 @@ os.environ["SPYGLASS_SORTING_DIR"] = "/stelmo/nwb/spikesorting"
 
 # import tables so that we can call them easily
 from spyglass.common import AnalysisNwbfile
+from spyglass.decoding.decoding_merge import DecodingOutput
 from spyglass.spikesorting import SpikeSorting
 
 
 def main():
     AnalysisNwbfile().nightly_cleanup()
     SpikeSorting().nightly_cleanup()
+    DecodingOutput().cleanup()
 
 
 if __name__ == "__main__":
