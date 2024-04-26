77b675a317a9b94f0caaeb6d9b899502988f7f8b
Jeremy Magland <jmagland@flatironinstitute.org>
2024-04-26 12:15:35 -0400

    fix h5pyfile import

--- src/spyglass/utils/__init__.py
+++ src/spyglass/utils/__init__.py
@@ -1,6 +1,6 @@
 from spyglass.utils.dj_merge_tables import _Merge
 from spyglass.utils.dj_mixin import SpyglassMixin
 from spyglass.utils.logging import logger
-from spyglass.utils.H5pyFile import H5pyFile
+from spyglass.utils.h5pyfile import H5pyFile
 
 __all__ = ["_Merge", "SpyglassMixin", "logger", "H5pyFile"]
