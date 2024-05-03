1eef1346546e159595f7650c1384754f5621432a
Jeremy Magland <jmagland@flatironinstitute.org>
2024-05-03 14:19:04 -0400

    SPYGLASS_LINDI env var

--- src/spyglass/utils/h5pyfile.py
+++ src/spyglass/utils/h5pyfile.py
@@ -1,7 +1,14 @@
-try:
-    # Try to get H5pyFile from lindi
-    # This is the same as h5py.File when file path does not end with .lindi.json
+import os
+
+
+# If SPYGLASS_LINDI is set to 1, use the lindi library to open NWB files.
+# Otherwise, use h5py. Note that even in LINDI mode, if the file path does not
+# end with ".lindi.json", then H5pyFile is equivalent to h5py.File. Thus, the
+# difference only matters when opening LINDI files.
+
+USE_LINDI = os.environ.get("SPYGLASS_LINDI", "0") == "1"
+
+if USE_LINDI:
     from lindi import File as H5pyFile  # noqa: F401
-except ImportError:
-    # Fall back to h5py (no support for .lindi.json files)
+else:
     from h5py import File as H5pyFile  # noqa: F401
