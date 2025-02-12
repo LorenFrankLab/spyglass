ba6b437529303c490e0cd9f27a37257264c1e9ef
CBroz1 <CBrozdowski@yahoo.com>
2025-02-12 15:13:36 -0600

    WIP: revise alter and populate scripts

--- franklab_scripts/alter_tables.py
+++ franklab_scripts/alter_tables.py
@@ -1,4 +1,13 @@
-"""Update the MySQL tables with the latest table definitions in each spyglass.common class."""
+"""Update the MySQL tables with the latest table definitions.
+
+NOTES:
+- import all classes for datajoint to resolve foreign key references
+- datajoint does not allow altering primary keys, indexes, or foreign keys
+- datajoint cannot resolve foreign key references of master from part table,
+    so part tables cannot be updated yet
+- until https://github.com/datajoint/datajoint-python/issues/943 is resolved,
+    some enum definitions will prompt alter() even without a change
+"""
 
 import importlib
 import inspect
@@ -6,42 +15,52 @@ import warnings
 
 import datajoint as dj
 
-# NOTE: for some reason, all classes need to be imported first for datajoint to be able to resolve foreign
-# key references properly in the code below
-from spyglass.common import *  # noqa: F401,F403
+from spyglass.common import *  # noqa: F401, F403
+from spyglass.decoding import *  # noqa: F401, F403
+from spyglass.lfp import *  # noqa: F401, F403
+from spyglass.linearization import *  # noqa: F401, F403
+from spyglass.mua import *  # noqa: F401, F403
+from spyglass.position import *  # noqa: F401, F403
+from spyglass.ripple import *  # noqa: F401, F403
+from spyglass.sharing import *  # noqa: F401, F403
+from spyglass.spikesorting import *  # noqa: F401, F403
+from spyglass.utils.database_settings import SHARED_MODULES
 
 
 def main():
+    """Run `alter` on all tables in the shared modules."""
     warnings.simplefilter("ignore", category=DeprecationWarning)
     warnings.simplefilter("ignore", category=FutureWarning)
-    module = importlib.import_module("spyglass.common")
+    for submodule in SHARED_MODULES:
+        update_submodule(submodule)
+
+
+def update_submodule(submodule):
+    """Run `alter` on all tables in `spyglass.submodule`."""
+    module = importlib.import_module(f"spyglass.{submodule}")
     for name, cls in inspect.getmembers(module, inspect.isclass):
         update_cls(cls)
         for attrname in dir(cls):
             attr = getattr(cls, attrname)
-            if isinstance(attr, type):
+            if isinstance(attr, type) and is_dj_table(attr):
                 update_cls(attr)
 
 
-def update_cls(cls):
-    if issubclass(
+def is_dj_table(cls):
+    """Return True if `cls` is a DataJoint table class."""
+    return issubclass(
         cls, (dj.Manual, dj.Lookup, dj.Imported, dj.Computed, dj.Part)
-    ):
-        print("Updating", cls)
-        try:
-            # NOTE: datajoint does not allow altering indexes yet
-            # this should affect only AnalysisNwbfile
-            # NOTE: datajoint cannot resolve foreign key references of master from part table yet,
-            # so part tables cannot be updated yet
-            # NOTE: until https://github.com/datajoint/datajoint-python/issues/943 is resolved,
-            # some enum definitions will prompt alter() even when there has not been a change
-            # NOTE: datajoint does not allow primary keys to be altered yet
-            # this includes changing the length of a varchar primary key
-            # (other tables use this primary key, so constraints would probably have to be removed and re-added)
-            cls.alter()
-        except Exception as e:
-            print("ERROR:", e)
-        print()
+    )
+
+
+def update_cls(cls):
+    """Run `alter` on `cls`, print all errors."""
+    print("Updating", cls)
+    try:
+        cls.alter()
+    except Exception as e:
+        print("ERROR:", e)
+    print()
 
 
 if __name__ == "__main__":
--- franklab_scripts/populate.py
+++ franklab_scripts/populate.py
@@ -1,22 +1,7 @@
-import os
-from pathlib import Path
-
 import spyglass as sg
 
 
 def main():
-    # CHANGE ME TO THE BASE DIRECTORY FOR DATA STORAGE ON YOUR SYSTEM
-    base_dir = Path("/stelmo/nwb")
-
-    os.environ["SPYGLASS_BASE_DIR"] = str(base_dir)
-    os.environ["KACHERY_STORAGE_DIR"] = str(base_dir / "kachery-storage")
-    os.environ["SPYGLASS_SORTING_DIR"] = str(base_dir / "spikesorting")
-
-    # session_id = "jaq_01"
-    # nwb_file_name = (sg.common.Session() & {"session_id": session_id}).fetch1(
-    #     "nwb_file_name"
-    # )
-
     sg.common.SpikeSorting().populate()
 
 
