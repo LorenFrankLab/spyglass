0854b3dc0907072d4f3b2b1651fad854b0576444
samuelbray32 <sam.bray@ucsf.edu>
2025-02-26 09:52:13 -0800

    make restriction in fetch_pose_dataframe compatible with export

--- src/spyglass/position/position_merge.py
+++ src/spyglass/position/position_merge.py
@@ -79,10 +79,13 @@ class PositionOutput(_Merge, SpyglassMixin):
         return query.fetch1_dataframe()
 
     def fetch_pose_dataframe(self):
-        """Fetch a single dataframe of pose bodypart coordinates.
+        """
+        Fetch a single dataframe of pose bodypart coordinates.
         This will work only for sources that have pose information.
         """
-        key = self.merge_restrict(self.proj()).proj()
+        key = self.merge_restrict(self.fetch("KEY", as_dict=True)).fetch(
+            "KEY", as_dict=True
+        )  # fetch the key from the merged table
         query = (
             source_class_dict[
                 to_camel_case(self.merge_get_parent(self.proj()).table_name)
