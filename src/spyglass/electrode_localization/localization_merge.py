575fadd8c3f98f265aeb01abe11499ca760c4cfa
Eric Denovellis <edeno@bu.edu>
2025-04-24 07:51:04 -0400

    Fix extra quote

--- src/spyglass/electrode_localization/localization_merge.py
+++ src/spyglass/electrode_localization/localization_merge.py
@@ -1,11 +1,16 @@
-from spyglass.utils import SpyglassMixin
 import datajoint as dj
+
+from spyglass.electrode_localization.v1.histology import (  # noqa: F401
+    ChannelBrainLocationHistologyV1,
+)
+from spyglass.electrode_localization.v1.micro_ct import (  # noqa: F401
+    ChannelBrainLocationMicroCTV1,
+)
+from spyglass.utils import SpyglassMixin
 from spyglass.utils.dj_merge_tables import _Merge
 
-from spyglass.electrode_localization.v1.micro_ct import ChannelBrainLocationMicroCTV1 # noqa: F401
-from spyglass.electrode_localization.v1.histology import ChannelBrainLocationHistologyV1 # noqa: F401
-
 schema = dj.schema("electrode_localization_v1")
+
 
 @schema
 class ChannelBrainLocation(_Merge, SpyglassMixin):
@@ -13,7 +18,7 @@ class ChannelBrainLocation(_Merge, SpyglassMixin):
         definition = """
         -> master
         -> ChannelBrainLocationHistologyV1
-        """"
+        """
 
     class MicroCTV1(SpyglassMixin, dj.Part):
         definition = """
