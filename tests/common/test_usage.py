e9d0b345b2a9b58f536b519a383390c6bbbc97a6
samuelbray32 <sam.bray@ucsf.edu>
2025-09-08 15:31:44 -0700

    update tests

--- tests/common/test_usage.py
+++ tests/common/test_usage.py
@@ -18,6 +18,7 @@ def gen_export_selection(
     pos_merge_tables,
     pop_common_electrode_group,
     common,
+    teardown,
 ):
     ExportSelection, _ = export_tbls
     pos_merge, lin_merge = pos_merge_tables
@@ -31,9 +32,9 @@ def gen_export_selection(
     ExportSelection.start_export(paper_id=1, analysis_id=3)
 
     _ = pop_common_electrode_group & "electrode_group_name = 1"
-    _ = common.IntervalPositionInfoSelection * (
-        common.IntervalList & "interval_list_name = 'pos 1 valid times'"
-    )
+    _ = trodes_pos_v1 * (
+        common.IntervalList & "interval_list_name = 'pos 0 valid times'"
+    )  # Note for PR: table and restriction change because join no longer logs empty results
 
     ExportSelection.start_export(paper_id=1, analysis_id=4)
 
@@ -42,12 +43,24 @@ def gen_export_selection(
     ).fetch1("KEY")
     (pos_merge & merge_key).fetch_nwb()
 
+    # ExportSelection.start_export(paper_id=1, analysis_id=5)
+    # projected_table = common.IntervalPositionInfoSelection.proj(
+    #     proj_interval_list_name="interval_list_name"
+    # )
+    # _ = projected_table & "proj_interval_list_name = 'pos 1 valid times'"
+
+    # ExportSelection.start_export(paper_id=1, analysis_id=6)
+    # _ = common.IntervalPositionInfoSelection & (
+    #     common.IntervalList & "interval_list_name = 'pos 1 valid times'"
+    # )
+
     ExportSelection.stop_export()
 
     yield dict(paper_id=1)
 
-    ExportSelection.stop_export()
-    ExportSelection.super_delete(warn=False, safemode=False)
+    if teardown:
+        ExportSelection.stop_export()
+        ExportSelection.super_delete(warn=False, safemode=False)
 
 
 def test_export_selection_files(gen_export_selection, export_tbls):
@@ -69,7 +82,9 @@ def test_export_selection_tables(gen_export_selection, export_tbls):
     assert len_tbl_2 == 1, "Selection tables not captured correctly"
 
 
-def test_export_selection_joins(gen_export_selection, export_tbls, common):
+def test_export_selection_joins(
+    gen_export_selection, export_tbls, common, trodes_pos_v1
+):
     ExportSelection, _ = export_tbls
     paper_key = gen_export_selection
 
@@ -83,9 +98,8 @@ def test_export_selection_joins(gen_export_selection, export_tbls, common):
         restr & {"table_name": common.ElectrodeGroup.full_table_name}
     ).fetch1("restriction"), "Export restriction not captured correctly"
 
-    assert "pos 1 valid times" in (
-        restr
-        & {"table_name": common.IntervalPositionInfoSelection.full_table_name}
+    assert "pos 0 valid times" in (
+        restr & {"table_name": trodes_pos_v1.full_table_name}
     ).fetch1("restriction"), "Export join not captured correctly"
 
 
@@ -113,21 +127,24 @@ def tests_export_selection_max_id(gen_export_selection, export_tbls):
 
 
 @pytest.fixture(scope="session")
-def populate_export(export_tbls, gen_export_selection):
+def populate_export(export_tbls, gen_export_selection, teardown):
     _, Export = export_tbls
     Export.populate_paper(**gen_export_selection)
     key = (Export & gen_export_selection).fetch("export_id", as_dict=True)
 
     yield (Export.Table & key), (Export.File & key)
 
-    Export.super_delete(warn=False, safemode=False)
+    if teardown:
+        Export.super_delete(warn=False, safemode=False)
 
 
 def test_export_populate(populate_export):
     table, file = populate_export
 
-    assert len(file) == 4, "Export tables not captured correctly"
-    assert len(table) == 39, "Export files not captured correctly"
+    assert len(file) == 4, "Export files not captured correctly"
+    assert (
+        len(table) == 37
+    ), "Export tables not captured correctly"  # Note for PR: Update because not using common.IntervalPositionInfoSelection (and param table)
 
 
 def test_invalid_export_id(export_tbls):
