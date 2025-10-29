4f39e29490b076439c4de76668abe92e7a82e323
CBroz1 <CBrozdowski@yahoo.com>
2025-10-29 14:52:25 -0500

    Remove old no-transaction-make, update docs for tripart approach

--- src/spyglass/utils/dj_mixin.py
+++ src/spyglass/utils/dj_mixin.py
@@ -68,8 +68,6 @@ class SpyglassMixin(ExportMixin):
     _banned_search_tables = set()  # Tables to avoid in restrict_by
     _parallel_make = False  # Tables that use parallel processing in make
 
-    _use_transaction = True  # Use transaction in populate.
-
     def __init__(self, *args, **kwargs):
         """Initialize SpyglassMixin.
 
@@ -653,63 +651,25 @@ class SpyglassMixin(ExportMixin):
 
         Supersedes datajoint.table.Table.populate for classes with that
         spawn processes in their make function and always use transactions.
-
-        `_use_transaction` class attribute can be set to False to disable
-        transaction protection for a table. This is not recommended for tables
-        with short processing times. A before-and-after hash check is performed
-        to ensure upstream tables have not changed during populate, and may
-        be a more time-consuming process. To permit the `make` to insert without
-        populate, set `_allow_insert` to True.
         """
         processes = kwargs.pop("processes", 1)
 
-        # Decide if using transaction protection
-        use_transact = kwargs.pop("use_transaction", None)
-        if use_transact is None:  # if user does not specify, use class default
-            use_transact = self._use_transaction
-            if self._use_transaction is False:  # If class default is off, warn
-                logger.warning(
-                    "Turning off transaction protection this table by default. "
-                    + "Use use_transation=True to re-enable.\n"
-                    + "Read more about transactions:\n"
-                    + "https://docs.datajoint.io/python/definition/05-Transactions.html\n"
-                    + "https://github.com/LorenFrankLab/spyglass/issues/1030"
-                )
-        if use_transact is False and processes > 1:
-            raise RuntimeError(
-                "Must use transaction protection with parallel processing.\n"
-                + "Call with use_transation=True.\n"
-                + f"Table default transaction use: {self._use_transaction}"
-            )
+        # Deprecate no transaction protection kwarg
+        if kwargs.pop("use_transaction", None) is not None:
+            from spyglass.common.common_usage import ActivityLog
+
+            ActivityLog().deprecate_log("populate no transaction")
 
         # Get keys, needed for no-transact or multi-process w/_parallel_make
         keys = [True]
-        if use_transact is False or (processes > 1 and self._parallel_make):
+        if processes > 1 and self._parallel_make:
             keys = (self._jobs_to_do(restrictions) - self.target).fetch(
                 "KEY", limit=kwargs.get("limit", None)
             )
 
-        if use_transact is False:
-            upstream_hash = self._hash_upstream(keys)
-            if kwargs:  # Warn of ignoring populate kwargs, bc using `make`
-                logger.warning(
-                    "Ignoring kwargs when not using transaction protection."
-                )
-
         if processes == 1 or not self._parallel_make:
-            if use_transact:  # Pass single-process populate to super
-                kwargs["processes"] = processes
-                return super().populate(*restrictions, **kwargs)
-            else:  # No transaction protection, use bare make
-                for key in keys:
-                    self.make(key)
-                if upstream_hash != self._hash_upstream(keys):
-                    (self & keys).delete(safemode=False)
-                    logger.error(
-                        "Upstream tables changed during non-transaction "
-                        + "populate. Please try again."
-                    )
-                return
+            kwargs["processes"] = processes
+            return super().populate(*restrictions, **kwargs)
 
         # If parallel in both make and populate, use non-daemon processes
         # package the call list
