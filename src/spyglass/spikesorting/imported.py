15c7b36f047bf2973a7a14450e56524551cb6e32
CBroz1 <CBrozdowski@yahoo.com>
2026-01-13 15:47:39 -0800

    Normalize deprecate_log calls

--- src/spyglass/spikesorting/imported.py
+++ src/spyglass/spikesorting/imported.py
@@ -1,5 +1,3 @@
-import copy
-
 import datajoint as dj
 import pandas as pd
 import pynwb
@@ -67,9 +65,9 @@ class ImportedSpikeSorting(SpyglassIngestion, dj.Imported):
         # Add merge table integration
         orig_key = {"nwb_file_name": nwb_file_name}
 
-        from spyglass.spikesorting.spikesorting_merge import (  # noqa: F401
+        from spyglass.spikesorting.spikesorting_merge import (
             SpikeSortingOutput,
-        )
+        )  # noqa: F401
 
         part_name = SpikeSortingOutput._part_name(self.table_name)
         SpikeSortingOutput._merge_insert(
@@ -87,7 +85,7 @@ class ImportedSpikeSorting(SpyglassIngestion, dj.Imported):
         from spyglass.common.common_usage import ActivityLog
 
         ActivityLog().deprecate_log(
-            self, "ImportedSpikesorting.make", alt="insert_from_nwbfile"
+            name="ImportedSpikesorting.make", alt="insert_from_nwbfile"
         )
 
         return self.insert_from_nwbfile(key["nwb_file_name"])
